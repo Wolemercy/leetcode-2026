0ee5bb9d067c703f53292c0b2e0de71b52cec9d8
wolemercy <wolemercy@gmail.com>
2026-01-11 19:25:29 +0100

    Day 11: Maximum Rectangle

---
+++ jan/11-maximum-rectangle.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/maximal-rectangle
+
+# TODO: Review this problem again later
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        
+        cols = len(matrix[0])
+        heights = [0] * (cols + 1)
+        max_area = 0
+        
+        for row in matrix:
+            for i in range(cols):
+                if row[i] == "1":
+                    heights[i] += 1
+                else:
+                    heights[i] = 0
+            
+            stack = [-1]
+            for i in range(len(heights)):
+                while heights[i] < heights[stack[-1]]:
+                    h = heights[stack.pop()]
+                    w = i - stack[-1] - 1
+                    max_area = max(max_area, h * w)
+                stack.append(i)
+                
+        return max_area
